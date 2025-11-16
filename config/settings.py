15f241e293eef3a21baaf75e49e5d05f726b1106
omverma1810 <omverma1234q@gmail.com>
2025-11-17 04:13:15 +0530

    Fix static files storage for production

--- config/settings.py
+++ config/settings.py
@@ -97,7 +97,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'
